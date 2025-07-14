54c82e752ea661d2958e9e25f0cbfa9ae2b5d5d1
Gokulkrishnan3113 <gokulkrishnan3113@gmail.com>
2025-07-14 19:41:50 +0530

    added cron text route

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { updateInvoiceByIdHandler, getInvoiceByGstinHandler,getPendingInvoicesHandler } = require('../controllers/invoice');
+const { updateInvoiceByIdHandler, getInvoiceByGstinHandler, getPendingInvoicesHandler, triggerPendingInvoiceReminder } = require('../controllers/invoice');
 
 
 router.patch('/update-invoice/:gstin/:invoice_id', updateInvoiceByIdHandler);
 router.get('/invoices/:gstin', getInvoiceByGstinHandler);
 router.get('/pending-invoices/:gstin', getPendingInvoicesHandler);
+router.get('/test-reminder-cron', triggerPendingInvoiceReminder); // 👈 new route
 
 module.exports = router;
 
