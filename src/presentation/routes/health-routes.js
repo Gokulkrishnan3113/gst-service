2e88c4a1e606ea529350eea5d2439e29116289e5
Gokul Krishnan R <145863914+Gokulkrishnan3113@users.noreply.github.com>
2025-07-25 10:11:53 +0530

    Deleted gst-routes.js

--- src/presentation/routes/health-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { healthCheckHandler } = require('../controllers/health-controller');
-
-const router = express.Router();
-
-router.get('/', healthCheckHandler);
-
-module.exports = router;
