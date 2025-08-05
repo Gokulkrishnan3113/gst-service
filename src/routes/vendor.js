17b075ae7755de090944ec5826fa0596e97a1e00
Gokulkrishnan3113 <gokulkrishnan3113@gmail.com>
2025-08-05 12:42:10 +0530

    added decryption for add-mac endpoint

--- src/routes/vendor.js
+++ src/routes/vendor.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { getVendors, createVendor, modifyVendor, deleteVendor, appendMacToVendor } = require('../controllers/vendor');
 const { verifyDefaultApiKey, verifyAuth } = require('../middleware/apikeyverifier');
+const { decryptRequestBody } = require('../middleware/decrypt-body')
+
 
 router.get('/', verifyDefaultApiKey, getVendors); // GET /file-gst/vendors
 router.post('/', verifyDefaultApiKey, createVendor);
-router.post('/add-mac', verifyAuth, appendMacToVendor);
+router.post('/add-mac', decryptRequestBody, verifyAuth, appendMacToVendor);
 // router.patch('/:gstin', modifyVendor);
 // router.delete('/:gstin', deleteVendor);
 
